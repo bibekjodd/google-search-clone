3cd797e51deaf0caafe4804d17f4f8539faa5f25
bibekjodd <110604197+bibekjodd@users.noreply.github.com>
2022-10-11 11:28:21 +0545

    eslint rules updated

--- pages/search.js
+++ pages/search.js
@@ -11,7 +11,6 @@ import { HiOutlineDotsVertical } from 'react-icons/hi'
 import { API_KEY, CONTEXT_KEY } from '../key';
 import Head from 'next/head';
 import SearchResults from '../components/SearchResults';
-import Response from '../Respone'
 
 function search({ results }) {
   const router = useRouter();
@@ -40,10 +39,9 @@ function search({ results }) {
 }
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
   const startIndex = context.query.start || '0'
 
-  const data = useDummyData ? Response : await fetch(
+  const data = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
   ).then(response => response.json());
   return {
